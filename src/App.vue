a0716477970cf3ad69bc78dc7e11b413a9bda1d8
Artiom-Deyev <79644010+Artiom-Deyev@users.noreply.github.com>
2022-09-11 22:24:34 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header />
+  <router-view />
+  <Feedback />
+  <Footer />
+</template>
+
+<script>
+import Header from "../src/components/Header.vue";
+import Feedback from "../src/components/Feedback.vue";
+import Footer from "../src/components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Feedback,
+    Footer,
+  },
+};
+</script>
+
+<style>
+/*# sourceMappingURL=style.css.map */
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <Promo />
+  <Categories />
+  <Products />
+  <Advantages />
+</template>
+
+<script>
+// @ is an alias to /src
+import Promo from "../components/Promo.vue";
+import Categories from "../components/Categories.vue";
+import Products from "../components/Products.vue";
+import Advantages from "../components/Advantages.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Promo,
+    Categories,
+    Products,
+    Advantages,
+  },
+};
+</script>
